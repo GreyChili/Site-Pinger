1ef8f75f61a408d9dffdc11e0d56eec569c6fbb3
GreyChili <58607125+GreyChili@users.noreply.github.com>
2020-03-25 14:54:31 +0100

    Working GUI

--- ping.py
+++ ping.py
@@ -10,26 +10,27 @@ s = None
 
 
 
-def ping():
-    rep = os.system('ping ' + server_ip)  # Ping ip adress
+def ping(_server_ip, _url):
+    rep = os.system('ping ' + _server_ip)  # Ping ip adress
 
     # Give output
     if rep == 0:
-        status.configure(text=url + ' is up')
+        statusMsg = _url + ' is up'
     else:
-        status.configure(text=url + ' is down')
+        statusMsg = _url + ' is down'
 
-def getIP():
-    print('Getting IP')
-    if url != '':
-        print(url)
-    else:
-        print('No URL')
+    status.configure(text=statusMsg)
+    print(statusMsg)
 
     
+
+def getIP():
+    url = urlEntered.get()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
     server_ip = socket.gethostbyname(url)  # Get ip adress from url
-    ping()
+
+    ping(server_ip, url) # Ping
 
 
 # Create a window
@@ -37,15 +38,13 @@ window = tk.Tk()
 window.title('Site Pinger')
 window.minsize(250, 100)
 
-title = tk.Label(window, text='Enter URL') # Title
-urlEntered = tk.Entry(window) # URL Entry
-pingBtn = tk.Button(window, text='Ping', command=getIP) # Button
-status = tk.Label(window, text='Status') # Status of site
-
+title = tk.Label(window, text='ENTER URL') # Title
 title.pack()
+urlEntered = tk.Entry(window) # URL Entry
 urlEntered.pack()
-url = urlEntered.get()
+pingBtn = tk.Button(window, text='PING', command=getIP) # Button
 pingBtn.pack()
+status = tk.Label(window, width=20, text='STATUS') # Status of site
 status.pack()
 
 window.mainloop()
