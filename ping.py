427b2075cf66a8c19e2670d3d6c41da5ca635a5b
GreyChili <58607125+GreyChili@users.noreply.github.com>
2020-03-25 14:10:32 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,18 +1,51 @@
+import tkinter as tk
 import socket
 import sys
 import os
-import argparse
 
-parser = argparse.ArgumentParser(description='Ping a website by URL')
-parser.add_argument('-u', '--url', type=str, metavar='', required=True, help='URL to ping')
-args = parser.parse_args()
+url = ''
+server_ip = ''
+rep = None
+s = None
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
-server_ip = socket.gethostbyname(args.url) # Get ip adress from url
-rep = os.system('ping ' + server_ip) # Ping ip adress
 
-# Give output
-if rep == 0:
-    print('\n\n%s is up' %server_ip)
-else:
-    print('server is down')
+
+def ping():
+    rep = os.system('ping ' + server_ip)  # Ping ip adress
+
+    # Give output
+    if rep == 0:
+        status.configure(text=url + ' is up')
+    else:
+        status.configure(text=url + ' is down')
+
+def getIP():
+    print('Getting IP')
+    if url != '':
+        print(url)
+    else:
+        print('No URL')
+
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
+    server_ip = socket.gethostbyname(url)  # Get ip adress from url
+    ping()
+
+
+# Create a window
+window = tk.Tk()
+window.title('Site Pinger')
+window.minsize(250, 100)
+
+title = tk.Label(window, text='Enter URL') # Title
+urlEntered = tk.Entry(window) # URL Entry
+pingBtn = tk.Button(window, text='Ping', command=getIP) # Button
+status = tk.Label(window, text='Status') # Status of site
+
+title.pack()
+urlEntered.pack()
+url = urlEntered.get()
+pingBtn.pack()
+status.pack()
+
+window.mainloop()
